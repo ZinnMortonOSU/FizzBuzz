30f5b647f46601bb785fcbd40c1218a882ee8c60
Zinn Morton <mortonzi@oregonstate.edu>
2021-03-05 21:35:33 -0800

    Wrote second test for numbers that are a multiple of three but not 5

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -8,5 +8,10 @@ class TestFizzBuzz(unittest.TestCase):
         self.assertEqual(fizzbuzz.fizzbuzzNum(11), "11")
         self.assertEqual(fizzbuzz.fizzbuzzNum(98), "98")
 
+        #Multiples of 3 but not 5
+        self.assertEqual(fizzbuzz.fizzbuzzNum(3), "Fizz")
+        self.assertEqual(fizzbuzz.fizzbuzzNum(12), "Fizz")
+        self.assertEqual(fizzbuzz.fizzbuzzNum(99), "Fizz")
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
